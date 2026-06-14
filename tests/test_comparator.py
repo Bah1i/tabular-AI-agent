f945b2bd9a1ede51ada6b66050c2cd2acb2ac85b
Bah1i <bah2002ti@gmail.com>
2026-06-14 14:08:50 +0800

    Very large changes. Sql mode, ala lens, foofah benchmark...

--- tests/test_comparator.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from app.services.comparator import compare_dataframes
-def test_numeric_tolerance():
-    ok, _ = compare_dataframes(pd.DataFrame({'x':[1.0000001]}), pd.DataFrame({'x':[1.0]})); assert ok
-def test_date_tolerance():
-    ok, _ = compare_dataframes(pd.DataFrame({'date':['10.01.2025']}), pd.DataFrame({'date':['2025-01-10']})); assert ok
